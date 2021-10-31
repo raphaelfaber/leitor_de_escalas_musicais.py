1a8c0c258c5b291ba6c4fe4109b064add6ca6b8b
Raphael Faber <raphael.f@live.com>
2021-10-30 23:41:37 -0300

    v3

--- nota1.py
+++ nota1.py
@@ -1,29 +1,44 @@
-def escalador(nota):
+def escalador():
     notas = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
     notasr = []
-    try:
-        while True:
-            p = nota.upper()
-            if p not in 'A#B#C#D#E#F#G#':
-                print('Nota musical inválida')
-                continue
-            else:
-                break
-    except:
-        print('Nota musical inválida')
+    while True:
+        p = input('Digite uma nota musical: ').upper()
+        if p not in 'A#B#C#D#E#F#G#':
+            print('Nota musical inválida')
+            try:
+                p = str(input('Digite uma nota válida: ')).upper()
+            except:
+                print('Nota digitada inválida')
+            break
+        else:
+            break
     for nota in range(0,15):
         if notas[nota] == p:
-            posição = nota
+            posicao = nota
             break
-    for c in range (posição,posição+12):
+    for c in range (posicao,posicao+12):
         notasr.append(notas[c])
     notasr = notasr*2
     return notasr
 
 
 def escalamaior(notasr):
-      print(f'A escala maior de {notasr[0]} é: {notasr[0]} {notasr[0+2]} {notasr[0+4]} {notasr[0+5]} {notasr[0+7]} {notasr[0+9]} {notasr[0+11]} ')
+      print(f'A escala maior de {notasr[0]} é: {notasr[0]} - {notasr[0+2]} - {notasr[0+4]} - {notasr[0+5]} - {notasr[0+7]} - {notasr[0+9]} - {notasr[0+11]} ')
 
 
 def escalamenor(notasr):
-    print(f'A escala menor de {notasr[0]} é: {notasr[0]} {notasr[0 + 2]} {notasr[0 + 3]} {notasr[0 + 5]} {notasr[0 + 7]} {notasr[0 + 8]} {notasr[0 + 10]}')
+    print(f'A escala menor de {notasr[0]} é: {notasr[0]} - {notasr[0 + 2]} - {notasr[0 + 3]} - {notasr[0 + 5]} - {notasr[0 + 7]} - {notasr[0 + 8]} - {notasr[0 + 10]}')
+
+
+def harmonicomaior(notasr):
+    print(f'O campo hamônico maior do {notasr[0]} é: {notasr[0]} - {notasr[0+2]}m - {notasr[0+4]}m - {notasr[0+5]} - {notasr[0+7]} - {notasr[0+9]}m - {notasr[0+11]}dim/7ª ')
+
+
+def texto(mensagem,tamanho=50):
+    print( '=' * tamanho)
+    print(f'{mensagem:^{tamanho}}')
+    print( '=' * tamanho)
+
+
+def linha(tamanho=50):
+    print('-'*tamanho)
--- programa_principal.py
+++ programa_principal.py
@@ -1,21 +1,31 @@
-notas = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
-notasr = []
-try:
-    while True:
-        p = str(input('digite uma nota musical: ')).upper()
-        if p not in 'A#B#C#D#E#F#G#':
-            print('Nota musical inválida')
-            continue
-        else:
+from nota1 import*
+from time import sleep
+while True:
+    texto('Bem vindo ao gerador de escalas musicais!')
+    print("""Para usar esse programa considere:
+    C - Dó
+    D - Ré
+    E - Mí
+    F - Fá
+    G - Sol
+    A - Lá
+    B - Si""",end='\n')
+    p=escalador()
+    sleep(1)
+    linha()
+    escalamaior(p)
+    linha()
+    sleep(1)
+    escalamenor(p)
+    linha()
+    sleep(1)
+    harmonicomaior(p)
+    try:
+        q = str(input('Deseja sair?')).lower()
+    except:
+        print(('Digite uma opção válida!'))
+    else:
+        if q in 'sy':
             break
-except:
-    print('Nota musical inválida')
-for nota in range(0,15):
-    if notas[nota] == p:
-        posição = nota
-        break
-for c in range (posição,posição+12):
-    notasr.append(notas[c])
-notasr = notasr*2
-print(f'A escala maior de {notasr[0]} é: {notasr[0]} {notasr[0+2]} {notasr[0+4]} {notasr[0+5]} {notasr[0+7]} {notasr[0+9]} {notasr[0+11]} ')
-print(f'A escala menor de {notasr[0]} é: {notasr[0]} {notasr[0+2]} {notasr[0+3]} {notasr[0+5]} {notasr[0+7]} {notasr[0+8]} {notasr[0+10]}')
+        else:
+            continue
